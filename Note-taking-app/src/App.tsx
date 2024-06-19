4719a595421f8dfc346f604c3c312185f9239d1c
Emily Wahlin <emilysaletti@gmail.com>
2024-06-20 00:35:53 +0200

    added tags, form

--- Note-taking-app/src/App.tsx
+++ Note-taking-app/src/App.tsx
@@ -1,6 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Navigate, Route, Routes } from "react-router-dom"
+import { NewNote } from "./NewNote"
+
+
+export type Note = {
+	id: string
+} & NoteData
+
+export type NoteData = {
+	title: string
+	markdown: string
+	tags: Tag []
+}
+
+export type Tag = {
+	id: string
+	label: string
+}
 
 
 
@@ -11,7 +28,7 @@ function App() {
 	<Container>
 	<Routes>
 		<Route path ="/" element={<h1>Home</h1>} />
-		<Route path ="/new" element={<h1>New</h1>} />
+		<Route path ="/new" element={<NewNote/>} />
 		<Route path ="/:id">
 			<Route index element ={<h1>Show</h1>} />
 			<Route path="edit" element ={<h1>Edit</h1>} />
