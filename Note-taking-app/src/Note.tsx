c3a3f6a7e389638022e7a6e247afef91759add4a
Emily Wahlin <emilysaletti@gmail.com>
2024-06-30 23:30:36 +0200

    added buttons for delete,edit and back

--- Note-taking-app/src/Note.tsx
+++ Note-taking-app/src/Note.tsx
@@ -1,5 +1,6 @@
-import { Badge, Col, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import { useNote } from "./NoteLayout";
+import { Link } from "react-router-dom";
 
 export function Note(){
 	const note = useNote()
@@ -23,6 +24,17 @@ export function Note(){
 			</Stack>
 				)}
 			</Col>
+			<Col xs="auto">
+			<Stack gap={2} direction="horizontal">
+				<Link to={`/${note.id}/edit`}>
+					<Button variant="primary">Edit</Button>
+				</Link>
+				<Button variant="outline-danger">Delete</Button>
+				<Link to="..">
+					<Button variant="outline-secondary">Back</Button>
+				</Link>
+			</Stack>
+		  </Col>
 		</Row>
 
 	</>
